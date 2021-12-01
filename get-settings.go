90bb063fda1636d8532fb250286469499e4eb38e
extra5ens <molotkov.md@phystech.edu>
2021-12-01 15:24:14 +0300

    Added simple program to show settings

---
+++ get-settings.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+func main() {
+	log.Println("I will delete your database")
+	log.Println(strings.Repeat("-", 37))
+	// Create Client
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+			"http://localhost:9201",
+		},
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: 5 * time.Second,
+			DialContext:           (&net.Dialer{Timeout: 5 * time.Second}).DialContext,
+			TLSClientConfig: &tls.Config{
+				MaxVersion:         tls.VersionTLS11,
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	es, err := elasticsearch.NewClient(cfg)
+
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	} else {
+		//log.Println(es.Info())
+	}
+	// Lest's scan settings frin
+	getset_res, err := es.Indices.GetSettings(
+		es.Indices.GetSettings.WithIndex("test"),
+		es.Indices.GetSettings.WithPretty(),
+	)
+	if err != nil {
+		log.Fatalf("index get setting error: %s", err)
+	} else {
+		log.Println(getset_res)
+	}
+	getset_res.Body.Close()
+	log.Println(strings.Repeat("-", 37))
+}
